e5fdcbd7995f70477db3047269fb843b254b7379
Ruben de Jager <r0851300@student.thomasmore.be>
2021-04-26 11:21:58 +0200

    first commit

---
+++ src/main/java/be/thomasmore/hummingbee/HummingbeeApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.hummingbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HummingbeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HummingbeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/hummingbee/HummingbeeApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.hummingbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HummingbeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
